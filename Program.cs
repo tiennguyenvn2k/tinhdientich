e1b03078a1c1be80028f5e9470cdec36119e3bcc
tiennguyenvn2k <hiiamgamer2000@gmail.com>
2023-12-06 21:25:05 +0700

    hoan thanh bai tap

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+namespace Usingoperator;
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        float chieurong;
+        float chieudai;
+        Console.WriteLine("Nhap thong so cho hinh chu nhat: ");
+        Console.WriteLine("Nhap chieu rong: ");
+        chieurong = float.Parse(Console.ReadLine());
+        Console.WriteLine("Nhap chieu dai: ");
+        chieudai = float.Parse(Console.ReadLine());
+        Console.WriteLine("Dien tich hinh chu nhat la: " + chieudai*chieurong);
+    }
+}
